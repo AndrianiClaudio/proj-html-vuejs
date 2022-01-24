974955a75ec3cd1f6bfc004f6385aadd5820706a
AndrianiClaudio <Claudioandriani1994@gmail.com>
2022-01-24 11:49:50 +0100

    Creazione:file stile, variabili colori, Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+      Header
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang='scss' scoped>
+header.header {
+    height: 750px;
+    background-image: url('../assets/img/home-page-slider.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+</style>
