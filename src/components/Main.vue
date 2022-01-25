ed73f3498e7c4d1a87dbbe1c09c91cb556fc4aad
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-01-25 15:51:51 +0100

    Main.vue ... ordine style

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,61 +1,33 @@
 <template>
   <main class="main">
-      <div
-      v-for="(section,index) in sections"
-      :key='`section-${index}`'
-      class="section-container"
-      >
-        <Section
-        :section = section
-        />
-      </div>
+    <div
+    v-for="(section,index) in sections"
+    :key='`section-${index}`'
+    class="section-container"
+    >
+      <Section
+      :section = section
+      />
+    </div>
   </main>
 </template>
 
 <script>
-// todo: inserire icon fontawesome
-// /* import the fontawesome core */
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// /* import specific icons */
-// import {
-//   faGlobe,
-//   faDollarSign,
-//   faTint,
-//   faStethoscope,
-//   faGraduationCap,
-// } from '@fortawesome/free-solid-svg-icons';
-// /* import font awesome icon component */
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
 import Section from './Section.vue';
-
-// /* add icons to the library */
-// library.add(
-//   faGlobe,
-//   faDollarSign,
-//   faTint,
-//   faStethoscope,
-//   faGraduationCap,
-// );
 
 export default {
   name: 'Main',
   components: {
-    // FontAwesomeIcon,
     Section,
   },
-  // methods: {
-  //   getNameFromId(i) {
-  //     return i.split('-').join(' ');
-  //   },
-  // },
   data() {
     return {
       sections: [
-        // every-moment-counts
+        // ---#every-moment-counts
         {
           id: 'every-moment-counts',
           containers: [
+            // container-left
             {
               name: 'container-left',
               title: true,
@@ -68,6 +40,8 @@ export default {
                 value: 'our mission',
               },
             },
+            // FINE container-left
+            // container-right
             {
               name: 'container-right',
               title: false,
@@ -107,9 +81,11 @@ export default {
                 },
               ],
             },
+            // FINE container-right
           ],
         },
-        // our-values
+        // FINE ---#every-moment-counts
+        // ---#our-values
         {
           id: 'our-values',
           containers: [
@@ -186,7 +162,8 @@ export default {
             },
           ],
         },
-        // recent-causes
+        // FINE ---#our-values
+        // --- #recent-causes
         {
           id: 'recent-causes',
           containers: [
@@ -215,7 +192,8 @@ export default {
             },
           ],
         },
-        // latest-articles
+        // FINE --- #recent-causes
+        // ---#latest-articles
         {
           id: 'latest-articles',
           containers: [
@@ -292,7 +270,7 @@ export default {
             },
           ],
         },
-
+        // FINE ---#latest-articles
       ],
     };
   },
@@ -300,154 +278,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/scss/partials/_variables.scss';
-@import '../assets/scss/partials/_mixins.scss';
-
-.section-container {
-  section {
-      @include flex();
-      .title {
-          :not(.no-uppercase) {
-              text-transform: uppercase;
-          }
-      }
-      hr {
-          color: $GoldenrodColor;
-          // distanzia riga tra titolo e paragrafi
-          margin: 1rem 0;
-          width: 200px;
-
-      }
-      .paragraphs {
-          p {
-              padding: 1rem 0;
-              line-height: 2rem;
-          }
-      }
-      button {
-          margin-top: 1rem;
-          padding: .5rem 1rem;
-          background-color: $GoldenrodColor;
-          border:1px solid $GoldenrodColor;
-          outline:1px solid $GoldenrodColor;
-          color: white;
-          text-transform: uppercase;
-      }
-      &#every-moment-counts {
-          .container-left {
-              //next...variable
-              width:70%;
-              padding: 5rem;
-          }
-          .container-right {
-              @include flex($dir:column,$jus:center);
-              //next..variable
-              width:30%;
-              //next..variable
-              @include bgSet('../assets/img/home-content-bg-1.jpg');
-              .counts-info {
-                  @include flex($ali:center,$jus:center,$dir: column);
-                  text-align: center;
-                  padding: 3rem;
-                  color: white;
-                  //all-icons
-                  svg,i {
-                      font-size: 1.5rem;
-                  }
-              }
-          }
-      }
-      &#our-values {
-          color: white;
-          @include bgSet('../assets/img/home-content-bg-2.jpg');
-          .container {
-              @include flex($dir:row,$jus:center);
-              flex-wrap: wrap;
-              padding: 5rem;
-              // margin: 0 auto;
-              .title {
-                  text-align: center;
-                  width:100%;
-                  hr {
-                      margin-left: auto;
-                      margin-right: auto;
-                  }
-              }
-              .cards {
-                  @include flex();
-                  // padding: 3.5rem;
-                  .our-values-info {
-                      @include flex($dir:column,$ali:center);
-                      text-align: center;
-                      padding: 1.5rem;
-                      .title{
-                          * {
-                              width: 100%;
-                          }
-                          svg {
-                              font-size: 3.5rem;
-                              color: $GoldenrodColor;
-                          }
-                      }
-                  }
-              }
-          }
-      }
-      &#recent-causes {
-          @include flex($ali:center,$jus:center);
-          text-align: center;
-          padding: 5rem;
-          hr {
-              margin-left: auto;
-              margin-right: auto;
-          }
-          .cards {
-              padding: 5rem;
-              @include flex($gap:1.5rem);
-          }
-      }
-      &#latest-articles {
-        .container {
-          margin: 0 auto;
-          width:100%;
-          padding: 5rem;
-          .title{
-            @include flex($dir:column);
-            width:100%;
-            h2,
-            .subtitle {
-              text-align: center;
-            }
-            hr {
-              margin: 1rem auto;
-            }
-          }
-          .cards {
-            padding: 3rem;
-            @include flex($gap: 2.5rem,$jus:center);
-            .subtitle {
-              text-transform: capitalize;
-              text-align: left;
-            }
-            .main-card {
-              @include flex($ali:flex-start,$jus: flex-start,$dir: column);
-              width: 600px;
-              }
-              .sub-cards-container {
-                // width: 100%;
-                .sub-card{
-                  padding-bottom: 2rem;
-                  flex-direction: row;
-                  display: flex;
-                  max-width: 600px;
-                }
-            }
-          }
-        }
-      }
-      &#change-a-life-today {
-        @include bgSet('../assets/img/home-footer.jpg');
-      }
-  }
-}
 </style>
--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,86 +1,90 @@
 <template>
-    <section :id = 'section.id'>
-        <div
-        v-for="(container,index) in section.containers"
-        :key='`container-${index}`'
-        :class="container.name"
+  <div :id = 'section.id' class="section">
+    <div
+    v-for="(container,index) in section.containers"
+    :key='`container-${index}`'
+    :class="container.name"
+    >
+      <!-- TITOLO SEZIONE -->
+      <div class="title" v-if="container.title">
+        <h2>{{getNameFromId(section.id)}}</h2>
+        <hr>
+        <span
+        v-if="container.subtitle"
+        class="subtitle no-uppercase"
         >
-        <div class="title" v-if="container.title">
-            <h2>{{getNameFromId(section.id)}}</h2>
-            <hr>
-            <span
-            v-if="container.subtitle"
-            class="subtitle no-uppercase"
-            >
-                {{container.subtitle}}
-            </span>
-        </div>
-        <div class="cards" v-if="container.cards">
-
-            <div
-            v-for="(card,index) in container.cards"
-            :key='`card-${index}`'
-            :class="card.name"
-            >
-                <!-- eslint-disable max-len -->
-                <img :src=card.img alt="inAlt" v-if="card.img">
-
-                <div class="title" v-if="card.title">
-                    <font-awesome-icon
-                    :icon='card.title.icon.name'
-                    v-if="card.title.icon && card.title.icon.pickedBy === 'fa'"
-                    />
-                    <!-- eslint-disable max-len -->
-                    <i
-                    :class="card.title.icon.class"
-                    v-if="card.title.icon && card.title.icon.pickedBy === 'w3_fa'"></i>
-                    <span>{{card.title.text}}</span>
-                    <div class="subtitle" v-if="card.subtitle">
-                        {{card.subtitle}}
-                    </div>
-                </div>
-                <div class="paragraphs" v-if="card.paragraphs">
-                    <p
-                    v-for="(p,index) in card.paragraphs"
-                    :key='`card-parag-${index}`'
-                    >
-                        {{p}}
-                    </p>
-                </div>
-                <button v-if="card.button">{{card.button.value}}</button>
+          {{container.subtitle}}
+        </span>
+      </div>
+      <!-- CARDS CONTENT -->
+      <div class="cards" v-if="container.cards">
+        <div
+        v-for="(card,index) in container.cards"
+        :key='`card-${index}`'
+        :class="card.name"
+        >
+          <!-- IMMAGINE CARD -->
+          <img :src=card.img alt="Errore caricamento immagine" v-if="card.img">
+          <!-- CONTAINER TITOLO CARD -->
+          <div class="title" v-if="card.title">
+            <!-- ICON -->
+            <font-awesome-icon
+            :icon='card.title.icon.name'
+            v-if="card.title.icon && card.title.icon.pickedBy === 'fa'"
+            />
+            <i
+            :class="card.title.icon.class"
+            v-if="card.title.icon && card.title.icon.pickedBy === 'w3_fa'"></i>
+            <!-- TITOLO CARD -->
+            <span class="title-txt">{{card.title.text}}</span>
+            <!-- SOTTOTITOLO CARD -->
+            <div class="subtitle" v-if="card.subtitle">
+              <span>{{card.subtitle}}</span>
             </div>
-            <div v-if="container.sub_cards" class="sub-cards-container">
-            <div
-            v-for="(subCard,index) in container.sub_cards"
-            :key='`sub-card-${index}`'
-            :class="subCard.name"
-            >
-                <!-- eslint-disable max-len -->
-                <img :src=subCard.img alt="inAlt" v-if="subCard.img">
-
-                <div class="title" v-if="subCard.title">
-                    <span>{{subCard.title.text}}</span>
-                    <div class="subtitle" v-if="subCard.subtitle">
-                        {{subCard.subtitle}}
-                    </div>
-                </div>
-            </div>
-
-            </div>
-
-        </div>
-        <div class="paragraphs" v-if="container.paragraphs">
+          </div>
+          <!-- CARD PARAGRAPHS -->
+          <div class="paragraphs" v-if="card.paragraphs">
             <p
-            v-for="(p,index) in container.paragraphs"
-            :key="`paragraph-${index}`"
+            v-for="(p,index) in card.paragraphs"
+            :key='`card-parag-${index}`'
             >
-                {{p}}
+              {{p}}
             </p>
+          </div>
+          <!-- CARD BUTTON -->
+          <button v-if="card.button">{{card.button.value}}</button>
         </div>
-        <button v-if="container.button">{{container.button.value}}</button>
+        <div v-if="container.sub_cards" class="sub-cards-container">
+          <div
+          v-for="(subCard,index) in container.sub_cards"
+          :key='`sub-card-${index}`'
+          :class="subCard.name"
+          >
+            <!-- IMG SUB-CARD -->
+            <img :src=subCard.img alt="inAlt" v-if="subCard.img">
+            <!-- TITOLO SUB-CARD -->
+            <div class="title" v-if="subCard.title">
+                <span class="title-txt">{{subCard.title.text}}</span>
+                <div class="subtitle" v-if="subCard.subtitle">
+                    {{subCard.subtitle}}
+                </div>
+            </div>
+          </div>
         </div>
-
-    </section>
+      </div>
+      <!-- PARAGRAPHS CONTENT -->
+      <div class="paragraphs" v-if="container.paragraphs">
+        <p
+        v-for="(p,index) in container.paragraphs"
+        :key="`paragraph-${index}`"
+        >
+          {{p}}
+        </p>
+      </div>
+      <!-- BUTTON CONTENT-->
+      <button v-if="container.button" class="btn">{{container.button.value}}</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -112,7 +116,6 @@ export default {
   },
   props: {
     section: Object,
-    // FontAwesomeIcon: component,
   },
   methods: {
     getNameFromId(i) {
@@ -125,150 +128,152 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/scss/partials/_variables.scss';
 @import '../assets/scss/partials/_mixins.scss';
-
-section {
-    @include flex();
-    .title {
-        :not(.no-uppercase) {
-            text-transform: uppercase;
-        }
+.section {
+  @include flex();
+  .container {
+    width: 100%;
+    .cards {
+        @include flex();
+      }
+  }
+  [class*='container'] {
+    padding: 5rem;
+  }
+  .title {
+    text-align: center;
+    h2 {
+      font-size: 1.75rem;
+      text-transform: uppercase;
     }
     hr {
-        color: $GoldenrodColor;
-        // distanzia riga tra titolo e paragrafi
-        margin: 1rem 0;
-        width: 200px;
-
+      color: $GoldenrodColor;
+      margin: 1.25rem auto;
+      border: 2px solid $GoldenrodColor;
+      width: 200px;
     }
-    .paragraphs {
-        p {
-            padding: 1rem 0;
-            line-height: 2rem;
-        }
+  }
+  .paragraphs {
+    p {
+      padding: 1rem 0;
+      line-height: 2rem;
     }
-    button {
-        margin-top: 1rem;
-        padding: .5rem 1rem;
-        background-color: $GoldenrodColor;
-        border:1px solid $GoldenrodColor;
-        outline:1px solid $GoldenrodColor;
-        color: white;
-        text-transform: uppercase;
+  }
+  button {
+    margin-top: 1rem;
+    padding: .5rem 1rem;
+    background-color: $GoldenrodColor;
+    border:1px solid $GoldenrodColor;
+    outline:1px solid $GoldenrodColor;
+    color: white;
+    text-transform: uppercase;
+  }
+  .cards {
+    padding: 5rem;
+  }
+  // // EVERY MOMENT COUNTS
+  &#every-moment-counts {
+    .title {
+      text-align: left;
     }
-    &#every-moment-counts {
-        .container-left {
-            //next...variable
-            width:70%;
-            padding: 5rem;
-        }
-        .container-right {
-            @include flex($dir:column,$jus:center);
-            //next..variable
-            width:30%;
-            //next..variable
-            @include bgSet('../assets/img/home-content-bg-1.jpg');
-            .counts-info {
-                @include flex($ali:center,$jus:center,$dir: column);
-                text-align: center;
-                padding: 3rem;
-                color: white;
-                //all-icons
-                svg,i {
-                    font-size: 1.5rem;
-                }
-            }
-        }
+    hr {
+      margin-left: 0;
     }
-    &#our-values {
-        color: white;
-        @include bgSet('../assets/img/home-content-bg-2.jpg');
-        .container {
-            @include flex($dir:row,$jus:center);
-            flex-wrap: wrap;
-            padding: 5rem;
-            // margin: 0 auto;
-            .title {
-                text-align: center;
-                width:100%;
-                hr {
-                    margin-left: auto;
-                    margin-right: auto;
-                }
-            }
-            .cards {
-                @include flex();
-                // padding: 3.5rem;
-                .our-values-info {
-                    @include flex($dir:column,$ali:center);
-                    text-align: center;
-                    padding: 1.5rem;
-                    .title{
-                        * {
-                            width: 100%;
-                        }
-                        svg {
-                            font-size: 3.5rem;
-                            color: $GoldenrodColor;
-                        }
-                    }
-                }
-            }
-        }
+    .container-left {
+      width:$everyCountsContainerWidth;
     }
-    &#recent-causes {
+    .container-right {
         @include flex($ali:center,$jus:center);
+        @include bgSet('../assets/img/home-content-bg-1.jpg');
+        width:calc(100% - $everyCountsContainerWidth);
+        .counts-info {
+          text-align: center;
+          padding: 1.25rem 0;
+          color: white;
+          //all-icons
+          svg,i {
+              font-size: 1.5rem;
+          }
+        }
+    }
+  }
+  // // OUR VALUES
+  &#our-values {
+    color: white;
+    @include bgSet('../assets/img/home-content-bg-2.jpg');
+    .container {
+      @include flex($dir:row,$jus:center);
+      flex-wrap: wrap;
+      padding: 5rem;
+      .title {
         text-align: center;
-        padding: 5rem;
-        hr {
-            margin-left: auto;
-            margin-right: auto;
-        }
-        .cards {
-            padding: 5rem;
-            @include flex($gap:1.5rem);
-        }
-    }
-    &#latest-articles {
-        .container {
-        margin: 0 auto;
         width:100%;
-        padding: 5rem;
-        .title{
-            @include flex($dir:column);
-            width:100%;
-            h2,
-            .subtitle {
-            text-align: center;
+      }
+      .cards {
+        .our-values-info {
+          @include flex($dir:column,$ali:center);
+          text-align: center;
+          padding: 1.5rem;
+          .title{
+            * {
+              width: 100%;
             }
-            hr {
-            margin: 1rem auto;
+            svg {
+              font-size: 3.5rem;
+              color: $GoldenrodColor;
             }
+          }
         }
-        .cards {
-            padding: 3rem;
-            @include flex($gap: 2.5rem,$jus:center);
-            .subtitle {
-            text-transform: capitalize;
-            text-align: left;
-            }
-            .main-card {
-            @include flex($ali:flex-start,$jus: flex-start,$dir: column);
-            width: 600px;
-            }
-            .sub-cards-container {
-                // width: 100%;
-                .sub-card{
-                padding-bottom: 2rem;
-                flex-direction: row;
-                display: flex;
-                max-width: 600px;
-                }
-            }
-        }
-        }
+      }
     }
-    &#change-a-life-today {
-        @include bgSet('../assets/img/home-footer.jpg');
+  }
+  // RECENT CAUSES
+  &#recent-causes {
+    @include flex($ali:center,$jus:center);
+    padding-top: 5rem;
+    .cards {
+      @include flex($gap:1.5rem);
     }
+  }
+  // LATEST ARTICLES
+  &#latest-articles {
+    *{
+        text-align: left;
+      }
+    .container {
+      .title{
+        @include flex($dir:column);
+        width:100%;
+        h2,
+        .subtitle {
+          text-align: center;
+        }
+      }
+      .cards {
+        @include flex($gap: 2.5rem,$jus:center);
+        padding: 3.5rem;
+        .title-txt {
+          color: $GoldenrodColor;
+        }
+        .subtitle {
+          text-align: left;
+        }
+        .main-card {
+          @include flex($dir: column);
+          width: 600px;
+        }
+        .sub-cards-container {
+          padding-top:0;
+          padding-bottom:0;
+          .sub-card{
+            @include flex();
+            max-width: 600px;
+            &:not(:last-child) {
+              padding-bottom: 2rem;
+            }
+          }
+        }
+      }
+    }
+  }
 }
 </style>
